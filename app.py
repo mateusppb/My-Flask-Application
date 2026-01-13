7921deab6aeb4c6fd75fb380b0a62dbf39ff5509
Mateus <mateuspimentapb@gmail.com>
2026-01-13 12:24:56 -0300

    add basic business rules and validation for items and stores inputs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 import uuid
 from flask import Flask, request
 from flask_smorest import abort
@@ -12,6 +13,16 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure name is included in the JSON"
+        )
+    
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"Store already exists.")
+
     store_id = uuid.uuid4().hex
     store = {**store_data, "id" : store_id}
     stores[store_id] = store
@@ -20,8 +31,25 @@ def create_store():
 @app.post("/item")
 def create_item():
     item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure price, store_id and name are included in the JSON"
+        )
+
+    for item in item.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"Item already exists.")
+
     if item_data["store_id"] not in stores:
-        return abort(404, message= "Store not found")
+        abort(404, message= "Store not found")
     
     item_id = uuid.uuid4().hex
     item = {**item_data, "id" : item_id}
