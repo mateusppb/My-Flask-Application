f9ddcbce94376db9e1a7cd0e10e4a93aa42a1a9f
Mateus <mateuspimentapb@gmail.com>
2026-01-13 11:59:51 -0300

    Initial commit: Flask REST API with Docker support

---
+++ app.py
@@ -0,0 +1,48 @@
+import uuid
+from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
+
+app = Flask(__name__)
+
+@app.get("/store")
+def get_stores():
+    return {"stores" : list(stores.values())}
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id" : store_id}
+    stores[store_id] = store
+    return store, 201
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return abort(404, message= "Store not found")
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id" : item_id}
+    items[item_id] = item
+    return item, 201
+
+@app.get("/item")
+def get_all_items():
+    return {"items" : list(items.values())}
+    
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return abort(404, message= "Store not found")
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return abort(404, message= "Item not found")
